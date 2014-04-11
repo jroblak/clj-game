b305162b19678a74922f3012958bc572016b9dc3
Justin Oblak <jroblak@gmail.com>
2014-04-11 17:46:39 -0400

    starting to factor out level stuff for multiple levels

--- desktop/src-common/clj_game/core.clj
+++ desktop/src-common/clj_game/core.clj
@@ -9,24 +9,11 @@
             [play-clj.g2d :refer :all]
             [play-clj.ui :refer :all]
             [clj-game.entities :as e]
-            [clj-game.utils :as u]))
+            [clj-game.utils :as u]
+            [clj-game.level :as l]))
 
-; declare our 'screens' / 'levels'
+; declare forward bindings for our 'screens' / 'levels'
 (declare clj-game main-screen text-screen title-screen)
-
-; custom update code for the game
-; handles removing and resetting entities
-(defn update-screen!
-  [screen entities]
-  (doseq [{:keys [x y height me? attack? to-destroy] :as entity} entities]
-    (when me?
-      (x! screen x)
-      (when (< y (- height)) ; when the players y is less than the maps height, reset
-        (set-screen! clj-game title-screen)))
-    (when-let [[tile-x tile-y] to-destroy] ; iterate through breakable tiles that have to-destroy set
-      (tiled-map-layer! (tiled-map-layer screen "breakable")
-                        :set-cell tile-x tile-y nil)))
-  (map #(dissoc % :to-destroy) entities))
 
 ; TITLE SCREEN
 (defscreen title-screen
@@ -59,47 +46,31 @@
 (defscreen main-screen
   :on-show
   (fn [screen entities]
-    (let [screen (->> (/ 1 u/pixels-per-tile)
-                      (orthogonal-tiled-map "level1.tmx")
-                      (update! screen :camera (orthographic) :renderer))
-          sheet (texture "player.png")
-          tiles (texture! sheet :split 32 32)
-          player-images (for [col [0 1 2 3 4]]
-                          (texture (aget tiles 0 col)))
-          enemy-images (for [col [0 1 2 3 4]]
-                          (texture (aget tiles 1 col)))]
-      (flatten (pvalues
-                (apply e/create-player player-images)
-                (for [object (map-layer! (map-layer screen "entities") :get-objects)]
-                  (apply e/create-baddy (conj enemy-images (map-object! object :get-rectangle))))))))
+    (l/on-show "level1" screen entities))
   :on-render
   (fn [screen entities]
-    (clear! 0.5 0.5 1 1) ; RGBA background color
-    (->> entities
-         (e/handle-ai)
-         (map #(->> %
-                    (e/move screen)
-                    (e/collide screen)
-                    (e/prevent-move screen)
-                    (e/animate screen)))
-         (e/handle-attacks screen)
-         (render! screen)
-         (update-screen! screen)))
+    (l/on-render screen entities))
   :on-resize
   (fn [{:keys [width height] :as screen} entities]
-    (orthographic! screen
-                   :set-to-ortho
-                   false
-                   (* u/vertical-tiles (/ width height))
-                   u/vertical-tiles))
+    (l/on-resize screen entities))
   :on-timer
   (fn [screen entities]
-    (case (:id screen)
-      :player-attack-cooldown (for [entity entities]
-                                (if (get entity :me?)
-                                  (assoc entity :can-attack? true)
-                                  entity))
-      nil)))
+    (l/on-timer screen entities)))
+
+; SECOND LEVEL SCREEN
+(defscreen second-screen
+  :on-show
+  (fn [screen entities]
+    (l/on-show "level2" screen entities))
+  :on-render
+  (fn [screen entities]
+    (l/on-render screen entities))
+  :on-resize
+  (fn [{:keys [width height] :as screen} entities]
+    (l/on-resize screen entities))
+  :on-timer
+  (fn [screen entities]
+    (l/on-timer screen entities)))
 
 ; FPS RENDERER
 (defscreen text-screen
